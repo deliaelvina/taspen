47f73a402a06fb347f962339a763d29518567eb4
IFCA Software <ifcasoftware@Hanys-Macbook-Pro.local>
2019-11-13 10:57:59 +0700

    rename splus jadi urban

--- ios/UrbanTests/UrbanTests.m
+++ ios/UrbanTests/UrbanTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface SplusTests : XCTestCase
+@interface UrbanTests : XCTestCase
 
 @end
 
-@implementation SplusTests
+@implementation UrbanTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
